f93430685f7c07654940c6dae635faa158687d2c
Michal Sloniewski <michal.sloniewski@smartcodecreations.com>
2025-07-25 17:29:40 +0200

    Added Employee class

---
+++ src/main/java/pl/kurs/watercontainers/models/Employee.java
@@ -0,0 +1,26 @@
+package pl.kurs.watercontainers.models;
+
+import java.math.BigDecimal;
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private BigDecimal salary;
+
+    public Employee(String firstName, String lastName, BigDecimal salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
